94d8d24f09ad6a59031e22f00b25199e57275da5
BenniePalm32 <baplandhq@gmail.com>
2025-02-13 20:43:16 -0600

    unfinished

---
+++ tablePrinter.py
@@ -0,0 +1,33 @@
+'''
+
+Table Printer: Takes a list of lists of strings and displays it in 
+a well-organized table with each column right-justified. 
+Assume that all the inner lists will contain the same number of strings.
+
+For example:
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+the printTable() funcion would print the following:
+
+   apples Alice  dogs
+  oranges   Bob  cats
+ cherries Carol moose
+   banana David goose
+
+'''
+
+
+tableData =  [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+def printTable(table):
+    columnWidth = [0] * len(table)
+    lenOfList = len(table[0])
+
+    for column in range(len(table)):
+        
+
